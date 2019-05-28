80b75603af0f0c87965c8b166ec15d71dbe2558a
Carlos Pérez <cperez@dlsi.ua.es>
2019-05-29 00:47:51 +0200

    589d632

---
+++ precache-manifest.8476a87419f82167e6001287ecbb00fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db73618d7544a5be7698a6a8d13d8f8",
+    "url": "/copymus/scanner/index.html"
+  },
+  {
+    "revision": "7d1c2679997412d0c0c2",
+    "url": "/copymus/scanner/static/css/2.8219ca2f.chunk.css"
+  },
+  {
+    "revision": "e11e00bda8d90999ec99",
+    "url": "/copymus/scanner/static/css/main.cbbfc95f.chunk.css"
+  },
+  {
+    "revision": "7d1c2679997412d0c0c2",
+    "url": "/copymus/scanner/static/js/2.4c0e6f33.chunk.js"
+  },
+  {
+    "revision": "e11e00bda8d90999ec99",
+    "url": "/copymus/scanner/static/js/main.a5d22423.chunk.js"
+  },
+  {
+    "revision": "e34a22e083ffe6a29bc5",
+    "url": "/copymus/scanner/static/js/runtime~main.9db75ff1.js"
+  },
+  {
+    "revision": "cfbdceaae339610635a5a3c4edecbe58",
+    "url": "/copymus/scanner/static/media/logo.cfbdceaa.png"
+  }
+]);
